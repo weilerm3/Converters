dfdf13bd65a6644fc6615c210d5fd881625f1b3f
matthewweiler <matthewweiler96@gmail.com>
2017-04-10 11:13:33 -0400

    Test Commit

---
+++ src/main/java/Converter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matthew
+ */
+
+import java.time.*;
+
+public class Converter {
+    
+    public double CurrencyConvert(double dollars){
+        return 0;
+    }
+    
+    public LocalTime TimeConvert(LocalTime currTime){
+        return null;
+    }
+    
+    public double DistanceConvert(double miles){
+        return 0;
+    }
+    
+    public double WeightConversion(double pounds){
+        return 0;
+    }
+    
+    public double SpeedConversion(double miles, double hours){
+        return 0;
+    }
+}
---
+++ src/test/java/ConverterTests.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Matthew
+ */
+public class ConverterTests {
+    
+    public ConverterTests() {
+    }
+
+    @Test
+    public void CurrencyTest() {
+    
+    }
+    
+    @Test
+    public void TimeTest() {
+    
+    }
+    
+    @Test
+    public void DistanceTest() {
+    
+    }
+    
+    @Test
+    public void WeightTest() {
+    
+    }
+    
+    @Test
+    public void SpeedTest() {
+    
+    }
+}
